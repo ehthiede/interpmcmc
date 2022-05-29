44a65ae0237ff7a3872edbe6c4ba3500ac9f6922
Erik Henning Thiede <ehthiede@gmail.com>
2022-05-29 16:42:33 -0400

    Initial routines for making PMF

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import
+from setuptools import find_packages, setup
+
+
+setup(
+    name='InterpMCMC',
+    version='0.0.0',
+    license='MIT',
+    description='Utitilitise for running MCMC on a potential defined by interpolation',
+    author='Erik Henning Thiede, ...',
+    author_email='ehthiede@gmail.com',
+    # url='https://github.com/risilab/Autobahn',
+    url='',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    python_requires='>=3.8',
+)
---
+++ src/interpmcmc/synthetic_ala.py
@@ -0,0 +1,57 @@
+import numpy as np
+from scipy.interpolate import RectBivariateSpline
+
+class InterpForceField(object):
+    def __init__(self, rbs):
+        super().__init__()
+        self.rbs = rbs
+
+    def U(self, x, y):
+        return self.rbs(x, y, grid=False)
+    
+    def F(self, x, y):
+        f_x = self.rbs(x, y, grid=False, dx=1) 
+        f_y = self.rbs(x, y, grid=False, dy=1) 
+        return f_x, f_y
+
+def build_padded_ax(dmn, npnts, pad_width):
+    dx = (dmn[1] - dmn[0])  / npnts_x
+    npnts_padded = npnts + 1 + 2 * pad_width
+    bottom = dmn[0] - pad_width * dx
+    top = dmn[1] + pad_width * dx
+    padded_edges = np.linspace(bottom, top, npnts_padded)
+    padded_ax = (padded_edges[1:] + padded_edges[:-1]) / 2.
+    return padded_ax
+    
+
+def pad_pmf(pmf, domain, pad_width = 3, clip_val=20):
+    dmn_range_x = domain[0]
+    dmn_range_y = domain[1]
+    npnts_x, npnts_y = pmf.shape
+    
+    # Build xax values for padded pmf
+    xax_padded = build_padded_ax(dmn_range_x, npnts_x, pad_width)
+    yax_padded = build_padded_ax(dmn_range_y, npnts_y, pad_width)
+    
+    # Avoid inf values in pmf
+    if clip_val is not None:
+        pmf = np.clip(pmf, None, clip_val)
+  
+    # PMF w. Padded PBCs
+    pd = ((pad_width, pad_width), (pad_width, pad_width))
+    padded_pmf = np.pad(pmf, pd, mode='wrap')
+ 
+    return padded_pmf, xax_padded, yax_padded
+    #stack the pmf to account for periodic boundary conditions.
+    
+def build_rbs(pmf, domain, pad_width = 3, clip_val=20.):
+    pmf, xax, yax = pad_pmf(pmf, domain, pad_width, clip_val=20.)
+    
+    rbs = RectBivariateSpline(xax, yax, pmf)
+    return rbs
+   
+def build_ala_forcefield():
+    pmf = np.load('mean_pmf.npy')
+    domain = ((-np.pi, np.pi), (-np.pi, np.pi))
+    rbs = build_rbs(pmf, domain)
+    return InterpForceField(rbs)
